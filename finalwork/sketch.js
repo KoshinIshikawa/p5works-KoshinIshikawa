98850bd1f6db0509248a5763c7a5de51a5012aaa
KoshinIshikawa <65202462+KoshinIshikawa@users.noreply.github.com>
2020-11-17 12:07:48 +0900

    碁盤を作る

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,14 @@
 // 最終課題を制作しよう
-
-function setup(){
-  createCanvas(windowWidth, windowHeight);
+function setup () {
+  createCanvas(400, 400);
 }
 
-function draw(){
-  background(160, 192, 255);
-}
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function draw () {
+  background(220);
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      fill(0, 140, 0);
+      rect(j * 50, i * 50, 50, 50);
+    }
+  }
 }
