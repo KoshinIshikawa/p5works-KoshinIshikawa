f967a8e4d30685301c79ed58882a21d90dfa4bad
KoshinIshikawa <65202462+KoshinIshikawa@users.noreply.github.com>
2020-11-23 13:07:37 +0900

    Modify finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -7,15 +7,15 @@ let row;
 let turn = 0;
 
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(360, 360);
 }
 
 function draw() {
   background(255);
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      line(j * 100, 0, j * 100, height);
-      line(0, i * 100, width, i * 100);
+      line(j * 100, 0, j * 100, 300);
+      line(0, i * 100, 300, i * 100);
       if (board[i][j] == 1) {
         fill(255);
         ellipse(i * 100 + 50, j * 100 + 50, 80, 80);
@@ -25,7 +25,29 @@ function draw() {
       }
     }
   }
+  if (turn == 1) {
+    textSize(32);
+    fill(0);
+    text("Turn: X", 30, 350);
+  } else if (turn == 2) {
+    textSize(32);
+    fill(0);
+    text("Turn: ○", 30, 350);
+  }
+
+  let result = checkWinner();
+  if (result != null && (result == 1 || result == 2)) {
+    noLoop();
+    let resultP = createP('');
+    resultP.style('font-size', '32pt');
+    if (result == 1) {
+      resultP.html(`○ wins!`);
+    } else {
+      resultP.html(`X wins!`);
+    }
+  }
 }
+
 
 function mousePressed() {
   turn = turn + 1;
@@ -40,6 +62,35 @@ function mousePressed() {
   } else if (turn == 2) {
     board[row][col] = 2;
   }
+}
+
+function equals3(a, b, c) {
+  return a == b && b == c && a != '';
+}
+
+function checkWinner() {
+  let winner = null;
+
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[i][0], board[i][1], board[i][2])) {
+      winner = board[i][0];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (equals3(board[0][i], board[1][i], board[2][i])) {
+      winner = board[0][i];
+    }
+  }
+
+  if (equals3(board[0][0], board[1][1], board[2][2])) {
+    winner = board[0][0];
+  }
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
+
+ return winner;
 }
 
 function crossmark(x1, y1, x2, y2){
